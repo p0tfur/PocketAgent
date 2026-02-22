28aec78658cd97e12af405e19baf36cf5af2bdbf
p0tfur <hrab.wesol@gmail.com>
2026-02-22 21:32:05 +0100

    debug: add postgres error logging to diagnose Failed query

--- web/src/lib/server/db/index.ts
+++ web/src/lib/server/db/index.ts
@@ -9,7 +9,14 @@ const client = postgres(env.DATABASE_URL, {
 	idle_timeout: 20,
 	max_lifetime: 60 * 5,
 	connect_timeout: 10,
-	prepare: false
+	prepare: false,
+	onnotice: (msg) => console.log('[DB notice]', msg),
+	debug: (connection, query, params, types) => {
+		console.log('[DB query]', query, params);
+	}
 });
+
+// Test connection on startup
+client`SELECT 1`.then(() => console.log('[DB] Connection OK')).catch((err) => console.error('[DB] Connection FAILED:', err));
 
 export const db = drizzle(client, { schema });
